9ce5b9e3359a5d40b2e8a44bf15067ca7107ecb9
JosueSDR <116617290+JosueSDR@users.noreply.github.com>
2024-06-08 12:29:14 -0500

    pokemonname

--- siu/src/Pokemon.jsx
+++ siu/src/Pokemon.jsx
@@ -3,17 +3,36 @@ import './App.css';
 
 function Pokemon() {
   const [pokemon, setPokemon] = useState(null);
+  const [pokemonId, setPokemonId] = useState(1); // Estado para el ID del Pokémon
+  const [inputValue, setInputValue] = useState(''); // Estado para el valor del input
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/150') // Cambia el número al ID del Pokémon que quieras obtener
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
       .then(response => response.json())
       .then(data => setPokemon(data))
       .catch(error => console.error('Error fetching Pokémon:', error));
-  }, []);
+  }, [pokemonId]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    setPokemonId(inputValue.toLowerCase()); // Convertir el valor a minúsculas para compatibilidad
+  };
 
   return (
     <div className="App">
       <header className="App-header">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Enter Pokémon ID or Name"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSearchClick}>Search</button>
+        </div>
         {pokemon ? (
           <div className="pokemon-container">
             <h1>{pokemon.name}</h1>
